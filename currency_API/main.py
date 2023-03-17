8bd054cd1e940aa19c8b893ff65a2662660e30a6
Inge M <ischek@protonmail.com>
2023-03-17 16:42:41 +0100

    feat(currency_api): add currency API

--- currency_API/main.py
+++ currency_API/main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_currency(in_currency, out_currency):
+    'Get currency rate'
     url = f"https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1"
     content = requests.get(url).text
     soup = BeautifulSoup(content, 'html.parser')
@@ -17,11 +18,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    'App home page'
     return '<h1>Currency Rate API</h1> <p>Example API URL: /api/v1/usd-eur</p>'
 
 @app.route('/api/v1/<input_curr>-<output_curr>')
 
 def api(input_curr, output_curr):
+    'Create API'
     rate = get_currency(input_curr, output_curr)
     result_dict = {"input_currency": input_curr, "output_currency": output_curr, "rate": rate}
     return jsonify(result_dict)
--- weather_forecast/main.py
+++ weather_forecast/main.py
@@ -1,19 +1,37 @@
-import variables
+"Module gets 3 hourly weather forecast for specific city for a period of several days"
+import os
+import json
 import requests
+from config import api_key
 
-apiKeyVar = variables.api_key
+# API keys are available from https://openweathermap.org/
+# Get the absolute path to the directory where the script is located
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Populate data text file with fields: city, time, temperature, condition
-# Get data for the next 5 days
-# Append to text file 'data.txt'
+# Define the relative path to the file
+RELATIVE_FILE_PATH = 'temp/data.txt'
 
-# NOTE need to wait a few hours for the API to become active
-def city_weather(city, forecast_days):
-    url = f"https:/api.openweathermap.org/data/2.5/weather?q={city}&appid=bbb6fa7e576aa4d728087673b64b7d08"
-    r = requests.get(url)
-    # create json object
-    weather_forecast = r.json()
+# Combine the script directory and relative file path to get the absolute file path
+absolute_file_path = os.path.join(SCRIPT_DIR, RELATIVE_FILE_PATH)
 
-    return weather_forecast
+def get_weather(city):
+    "Get weather forecast for specific city"
+    weather_url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
+    content = requests.get(weather_url).text
+    content_dict = json.loads(content)
 
-print(city_weather(city="london", forecast_days=4))
+    # creates python list
+    list_content = content_dict["list"]
+
+    # loop through list and get values
+    for w_entry in list_content:
+        time = w_entry['dt_txt']
+        temperature = w_entry['main']['temp']
+        condition = w_entry['weather'][0]['description']
+        # Append to file
+        file = open(absolute_file_path, 'a')
+        file.write(f'{city}, {time}, {temperature}, {condition}\n')
+        file.close()
+
+# call function
+get_weather('Amsterdam')
