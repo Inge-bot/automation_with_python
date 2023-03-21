7f8817934a1b6951189e3b6ea9fa4ff7969e6300
Inge M <ischek@protonmail.com>
2023-03-21 11:17:18 +0100

    feat(rename_sub_folders_files): find all subfolders and rename files within

--- pathlib/rename_sub_sub_folders.py
+++ pathlib/rename_sub_sub_folders.py
@@ -5,10 +5,16 @@ def rename_files_subdirectory(directory):
     root_dir = Path(directory)
     file_paths = root_dir.glob('**/*')
 
+    # walk operation through the directories
     for path in file_paths:
         if path.is_file():
+
+            # find all subfolders in root dir
+            subfolders = path.parts[1:-1]
+
             # create new filename
-            new_filename = f"{path.parts[-3]}_{path.parts[-2]}_{path.name}"
+            new_filename = '_'.join(subfolders) + '_' + path.name
+
             # create new path object and rename
             new_filepath = path.with_name(new_filename)
             path.rename(new_filepath)
