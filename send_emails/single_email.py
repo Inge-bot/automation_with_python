5291468223113e350e664706cbff1597d0b65adb
Inge M <ischek@protonmail.com>
2023-03-23 17:04:07 +0100

    feat(send_email): send single email

---
+++ send_emails/single_email.py
@@ -0,0 +1,26 @@
+"""
+Email clients uses SMTP to send/receive emails.
+Send single email with gmail client.
+Add email account details in a variables.py file
+"""
+import yagmail
+import os
+import variables
+
+SENDER = variables.EMAIL
+RECEIVER = variables.EMAIL
+
+def send_email():
+    "Sends single email"
+    subject = "This is the subject!"
+    contents = '''
+    Dear John Doe,
+    Here is the content of the email!
+    Kind regards,
+    Jane Austin
+    '''
+
+    yag = yagmail.SMTP(user=SENDER, password=variables.PASSWORD)
+    yag.send(to=RECEIVER, subject=subject, contents=contents)
+
+send_email()
