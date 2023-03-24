db894e05e55d427080efb6ed4ad515b0e24bce9f
Inge M <ischek@protonmail.com>
2023-03-24 13:08:43 +0100

    feat(send_email): send email from specific time

--- send_emails/single_email.py
+++ send_emails/single_email.py
@@ -5,6 +5,7 @@ Add email account details in a variables.py file
 """
 import yagmail
 import time
+from datetime import datetime as dt
 import variables
 
 SENDER = variables.EMAIL
@@ -20,8 +21,11 @@ def send_email():
     Jane Austin
     '''
     while True:
-        yag = yagmail.SMTP(user=SENDER, password=variables.PASSWORD)
-        yag.send(to=RECEIVER, subject=subject, contents=contents)
-        time.sleep(60)
+        now = dt.now()
+        if now.hour == 13 and now.minute == 7: # time when email is first sent
+            yag = yagmail.SMTP(user=SENDER, password=variables.PASSWORD)
+            yag.send(to=RECEIVER, subject=subject, contents=contents)
+            print("Email sent")
+            time.sleep(60) # email reoccures every 60 seconds
 
 send_email()
