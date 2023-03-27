e1f4e99e09662ee46fb9298b434f98c6e1f11650
Inge M <ischek@protonmail.com>
2023-03-27 14:00:28 +0200

    feat(send_email): send email from outlook

---
+++ send_emails/outlook_email.py
@@ -0,0 +1,33 @@
+"""
+Send email from Outlook
+"""
+import smtplib
+from email.message import EmailMessage
+import variables
+
+def outlook_email():
+    "send email from outlook with smtp server"
+    SENDER = variables.EMAIL_OUTLOOK
+    RECEIVER = variables.RECEIVER
+    PASSWORD = variables.PASSWORD_OUTLOOK
+
+    # create multiline message
+    message = """
+    Just wanted to say hi from my Outlook address.
+    Catch you later!
+    """
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = 'Hello from Outlook!'
+    msg['From'] = SENDER
+    msg['To'] = RECEIVER
+
+    # start smtp server to send email
+    server = smtplib.SMTP('smtp.office365.com', 587) # connect to port 587
+    print(server.starttls()) # start TLS encrypted protocol
+    print(server.login(SENDER, PASSWORD))
+    server.send_message(msg) # use msg library
+    print('email sent')
+    server.quit()
+
+outlook_email()
